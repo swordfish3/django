c7916fd4392364990f260b8404d378ceb21d146f
root <root@desktop>
2017-05-04 20:09:41 +0300

    api works and product info

--- pharmacy/app/urls.py
+++ pharmacy/app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = (
         url(r'^$', views.index, name='index'),
         url(r'^allproducts/$', login_required(views.all_products), ),
-        # url(r'^id/$', login_required(views.searchByID), name='id'),
+        url(r'^allproducts/(?P<pharmacyid>\d+)/$', login_required(views.product_info), ),
 
 )
--- pharmacy/app/views.py
+++ pharmacy/app/views.py
@@ -10,6 +10,13 @@ def index(request):
 
 def all_products(request):
     web_data = urlopen('http://test.hua.gr:8000/pharmacy/?format=json').read().decode('utf-8')
-    data = json.loads(web_data)
-    context = {'data': data}
+    alldata = json.loads(web_data)
+    context = {'alldata': alldata}
     return render(request, 'allproducts.html', context)
+
+
+def product_info(request, pharmacyid):
+    web_data = urlopen('http://test.hua.gr:8000/pharmacy/'+str(pharmacyid)+"/?format=json").read().decode('utf-8')
+    p_data = json.loads(web_data)
+    context = {'pdata': p_data}
+    return render(request, "product.html", context)
